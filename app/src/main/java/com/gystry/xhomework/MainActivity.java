94941ead8d13e23a01f53c3c177c7dc128f28e67
gystry <gystry@163.com>
2019-08-12 14:05:00 +0800

    feat:三维动画控件完成

--- app/src/main/java/com/gystry/xhomework/MainActivity.java
+++ app/src/main/java/com/gystry/xhomework/MainActivity.java
@@ -1,21 +1,37 @@
 package com.gystry.xhomework;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.gystry.xhomework.widget.CircleTextView;
+import com.gystry.xhomework.widget.CamreaView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CircleTextView viewById = (CircleTextView) findViewById(R.id.progress_circular);
-        ObjectAnimator schedule = ObjectAnimator.ofFloat(viewById, "schedule", 0.45f);
-        schedule.setDuration(1000);
-        schedule.setStartDelay(500);
-        schedule.start();
+        CamreaView viewById = (CamreaView) findViewById(R.id.progress_circular);
+        ObjectAnimator bottomFlipAnimator = ObjectAnimator.ofFloat(viewById, "bottomFlip", 30f);
+        bottomFlipAnimator.setDuration(1000);
+
+        ObjectAnimator topFlipAnimator = ObjectAnimator.ofFloat(viewById, "topFlip", -30f);
+        topFlipAnimator.setDuration(1000);
+        topFlipAnimator.setStartDelay(200);
+
+        ObjectAnimator rotationFlipAnimator = ObjectAnimator.ofFloat(viewById, "rotationFlip", 270f);
+        rotationFlipAnimator.setDuration(1000);
+        rotationFlipAnimator.setStartDelay(200);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playSequentially(bottomFlipAnimator, rotationFlipAnimator, topFlipAnimator);
+        animatorSet.setStartDelay(500);
+        animatorSet.reverse();
+        animatorSet.start();
     }
 }
--- app/src/main/java/com/gystry/xhomework/widget/CamreaView.java
+++ app/src/main/java/com/gystry/xhomework/widget/CamreaView.java
@@ -17,6 +17,9 @@ public class CamreaView extends View {
     private final float IMG_SPAN = 300;
     private Paint mPaint;
     private Camera camera;
+    private float topFlip = 0;
+    private float bottomFlip = 0;
+    private float rotationFlip = 0;
 
     public CamreaView(Context context) {
         this(context, null);
@@ -31,15 +34,40 @@ public class CamreaView extends View {
         init(context);
     }
 
+    public float getTopFlip() {
+        return topFlip;
+    }
+
+    public void setTopFlip(float topFlip) {
+        this.topFlip = topFlip;
+        invalidate();
+    }
+
+    public float getBottomFlip() {
+        return bottomFlip;
+    }
+
+    public void setBottomFlip(float bottomFlip) {
+        this.bottomFlip = bottomFlip;
+        invalidate();
+    }
+
+    public float getRotationFlip() {
+        return rotationFlip;
+    }
+
+    public void setRotationFlip(float rotationFlip) {
+        this.rotationFlip = rotationFlip;
+        invalidate();
+    }
+
     private void init(Context context) {
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         bitmap = Utils.getAvatar(getResources(), R.mipmap.ic_xiaoxin);
         camera = new Camera();
 
-        camera.rotateX(60);
-        camera.setLocation(0, 0, -4*getResources().getDisplayMetrics().density);
-
-
+//        camera.rotateX(bottomFlip);
+        camera.setLocation(0, 0, -4 * getResources().getDisplayMetrics().density);
     }
 
     @Override
@@ -52,19 +80,26 @@ public class CamreaView extends View {
         //上半部分
         canvas.save();
         canvas.translate(IMG_SPAN + bitmap.getWidth() / 2f, IMG_SPAN + bitmap.getHeight() / 2f);
-        canvas.rotate(-30);
-        canvas.clipRect(-bitmap.getWidth(),-bitmap.getHeight(),bitmap.getWidth(),0);
-        canvas.rotate(30);
+        canvas.rotate(-rotationFlip);
+        camera.save();
+        camera.rotateX(topFlip);//不是旋转到指定bottomFlip角度，而是旋转了bottomFlip角度，是累积的
+        camera.applyToCanvas(canvas);//应用camera的角度旋转
+        camera.restore();
+        canvas.clipRect(-bitmap.getWidth(), -bitmap.getHeight(), bitmap.getWidth(), 0);
+        canvas.rotate(rotationFlip);
         canvas.translate(-(IMG_SPAN + bitmap.getWidth() / 2f), -(IMG_SPAN + bitmap.getHeight() / 2f));
         canvas.drawBitmap(bitmap, IMG_SPAN, IMG_SPAN, null);
         canvas.restore();
         //下半部分
         canvas.save();
         canvas.translate(IMG_SPAN + bitmap.getWidth() / 2f, IMG_SPAN + bitmap.getHeight() / 2f);
-        canvas.rotate(-30);
+        canvas.rotate(-rotationFlip);
+        camera.save();
+        camera.rotateX(bottomFlip);//不是旋转到指定bottomFlip角度，而是旋转了bottomFlip角度，是累积的
         camera.applyToCanvas(canvas);//应用camera的角度旋转
-        canvas.clipRect(-bitmap.getWidth(),0,bitmap.getWidth(),bitmap.getHeight());
-        canvas.rotate(30);
+        camera.restore();
+        canvas.clipRect(-bitmap.getWidth(), 0, bitmap.getWidth(), bitmap.getHeight());
+        canvas.rotate(rotationFlip);
         canvas.translate(-(IMG_SPAN + bitmap.getWidth() / 2f), -(IMG_SPAN + bitmap.getHeight() / 2f));
         canvas.drawBitmap(bitmap, IMG_SPAN, IMG_SPAN, null);
         canvas.restore();
